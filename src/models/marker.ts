70c012d00657538a50b88a1ad4c95328b1488b42
skeeDayo <steven_escopete@dlsu.edu.ph>
2023-12-12 16:25:08 +0800

    add create marker endpoint

---
+++ src/models/marker.ts
@@ -0,0 +1,10 @@
+export type markerObject = {
+    id: number;
+    image_link: string;
+    location: string;
+    addi_desc: string;
+    coordinates: {
+        x: number;
+        y: number;
+    };
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import type { markerObject } from '../models/marker';
 const db = require('../db/conn.ts');
 
 const router = express.Router()
@@ -19,6 +20,23 @@ router.get('/get-marker-info/:id', async (req, res) => {
     try {
         const result = await db.query('SELECT * FROM bidet WHERE id = ' + req.params.id);
         res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+router.get('/add-marker', async (req, res) => {
+    const marker: markerObject = req.body;
+
+    try {
+        const lastId = await db.query('SELECT MAX(id) FROM bidet')
+        const newId = lastId.rows[0].max + 1;
+
+        db.query(`INSERT INTO bidet 
+        (id, image_link, location, addi_desc, coordinates) VALUES
+        ($1, $2, $3, $4, $5)`,
+        [newId, marker.image_link, marker.location, marker.addi_desc, marker.coordinates]);
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
